ec4d27fff1fa3fd84c66b3a56f09dbb45f5aae1c
Abhijeet kukreti <129980752+abhikukreti@users.noreply.github.com>
2023-09-13 20:35:31 +0530

    Decision Tree.py

--- Decision Tree.py
+++ Decision Tree.py
@@ -17,7 +17,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 clf = DecisionTreeClassifier() //classifier model
 
-
 clf.fit(X_train, y_train)
 
 y_pred = clf.predict(X_test)//predict 
